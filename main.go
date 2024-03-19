646c18b207bc236ab46ae8c41cb013a0ddf80abc
nite07 <admin@nite07.com>
2024-03-19 23:35:13 +0800

    fix: version cmd

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	"sub2sing-box/cmd"
 )
 
-var Version string
-
-func init() {
-	Version = "dev"
-	cmd.SetVersion(Version)
-}
-
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
