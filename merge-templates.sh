59ccf8a3c760d51a4607cee87bd49021e3072bc2
nite <admin@nite07.com>
2025-02-12 23:19:28 +0800

    modify default templates

--- merge-templates.sh
+++ merge-templates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sing-box merge -C ./templates/windows ./templates/example-windows.json
 sing-box merge -C ./templates/linux ./templates/example-linux.json
-sing-box merge -C ./templates/android ./templates/example-android.json
+sing-box merge -C ./templates/windows ./templates/example-android.json
