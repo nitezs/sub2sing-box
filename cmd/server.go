876561eb6fb4734b5499387ad5348fa9afc2ac54
legiz-ru <legiz.ru@gmail.com>
2025-02-16 23:28:01 +0300

    add bind option

--- cmd/server.go
+++ cmd/server.go
@@ -6,18 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var port uint16
+var (
+	port uint16
+	bind string
+)
 
 func init() {
 	runCmd.Flags().Uint16VarP(&port, "port", "p", 8080, "server port")
+	runCmd.Flags().StringVarP(&bind, "bind", "b", "0.0.0.0", "bind address (e.g., 0.0.0.0, 127.0.0.1, localhost)")
 	RootCmd.AddCommand(runCmd)
 }
 
-var runCmd = &cobra.Command{
++var runCmd = &cobra.Command{
 	Use:   "server",
 	Long:  "Run the server",
 	Short: "Run the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		api.RunServer(port)
+		api.RunServer(bind, port)
 	},
 }
